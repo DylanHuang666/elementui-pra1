afcd4066a0a46d1f0a09665c56c1b7edc01cade2
Dylan <huanglinjay123@outlook.com>
2021-12-02 15:37:39 +0800

    标题组件；css3 transform案例

---
+++ src/components/titleBar.vue
@@ -0,0 +1,48 @@
+<template>
+<!-- 标题组件 -->
+  <div class="title">
+    <div class="title-box" ref="a">
+      <span>{{options.title}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Object,
+      default: function() {
+        return {
+          title: "标题",
+          leftColor: "red",
+          leftWith: "10",
+          fontSize: "16",
+          space: "20"
+        };
+      }
+    }
+  },
+  mounted() {
+    console.log("el", this.$refs.a.children);
+    const el = this.$refs.a
+    const span1 = this.$refs.a.children[0]
+    el.style.borderLeft = `${this.options.leftWith}px solid ${this.options.leftColor}`
+    span1.style.fontSize = `${this.options.fontSize}px`
+    span1.style.marginLeft = `${this.options.space}px`
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  display: inline-block;
+  &-box {
+    // border-left: 10px solid rgb(13, 130, 240);
+    span {
+      font-size: 18px;
+      margin-left: 20px;
+    }
+  }
+}
+</style>
--- src/views/manage-system-layout/pages/home.vue
+++ src/views/manage-system-layout/pages/home.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
-    <clock />
+    <!-- <clock /> -->
+    <titleBar :options="options" />
   </div>
 </template>
 
 <script>
-import clock from '../components/clock'
+import clock from "../components/clock";
+import titleBar from "@/components/titleBar";
+
 export default {
-  components:{
-    clock
+  components: {
+    clock,
+    titleBar
+  },
+  data() {
+    return {
+      options: {
+        title: "标题333",
+        leftColor: "red",
+        leftWith: "10",
+        fontSize: "16",
+        space: "20"
+      }
+    };
   }
 };
 </script>
