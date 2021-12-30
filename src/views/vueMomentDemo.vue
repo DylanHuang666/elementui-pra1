ec0562ae4a11d8d3be57c9e6bf139a45515236d4
Dylan <huanglinjay123@outlook.com>
2021-12-30 18:04:05 +0800

    更改

--- src/views/vueMomentDemo.vue
+++ src/views/vueMomentDemo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <!-- 只能在模板当过滤器用 -->
-    <span>{{date1|moment("YYYY-MM-DD HH:mm:ss")}}</span>
-    <div>{{d1}}</div>
+    <!-- vue-moment只能在模板当过滤器用 -->
+    <span>vue-moment: {{date1|moment("YYYY-MM-DD HH:mm:ss")}}</span>
+    <div>moment.js: {{d1}}</div>
   </div>
 </template>
 
@@ -22,6 +22,7 @@ export default {
     // console.log('this.$moment',this.$moment)
     this.d1 = moment(this.date1).format("YYYY-MMMM-DD dddd HH:mm:ss a")
     console.log('moment',this.d1)
+    console.log('123',moment('20220809').format("YYYY-MM-DD"))
   }
 }
 </script>
